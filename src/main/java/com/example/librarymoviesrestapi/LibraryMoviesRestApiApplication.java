11f56ee929c7815a60207f1987ff40924c27af87
Arek <arek1990cda@gmail.com>
2024-01-03 10:44:43 +0100

    initialization project

---
+++ src/main/java/com/example/librarymoviesrestapi/LibraryMoviesRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.librarymoviesrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryMoviesRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryMoviesRestApiApplication.class, args);
+    }
+
+}
